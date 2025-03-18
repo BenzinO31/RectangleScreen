f4cf2a8b2f5aca8f6711c63f0c45f6f0dfd51f3d
Evgeny Zhurilin <zhurilin2014@yandex.ru>
2025-03-18 13:26:37 +0200

    Initial Commit

---
+++ RectanglesScreen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RectanglesScreen
+//
+//  Created by Macbook on 18.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
